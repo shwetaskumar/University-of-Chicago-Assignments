dc93b1da8a9bfccfc500750f4ab4975a5b2d317e
Shweta Sampath Kumar <shwetaskumar96@gmail.com>
2023-05-21 17:56:45 -0500

    Update

--- BMI/app.py
+++ BMI/app.py
@@ -9,7 +9,7 @@ import urllib.request
 
 # Load your trained models
 bmi_model = tf.keras.models.load_model('BMI/best_bmi_model_v3_3.h5', compile=False)
-gender_model = tf.keras.models.load_model('best_gender_model_v1_0.h5')
+gender_model = tf.keras.models.load_model('BMI/best_gender_model_v1_0.h5')
 
 gender_labels = ["Female","Male"]
 
